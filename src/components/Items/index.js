6edcdc7c57700e71e5c4403937c126f70194b4d1
Wink007 <wink1301alex@gmail.com>
2018-02-20 22:49:20 +0200

    add cfg improved items for Items

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -3,11 +3,13 @@ import ItemInfo from './ItemInfo';
 import MainItems from './MainItems';
 import ImprovedItems from './ImprovedItems';
 
-
+//import mainItems from 'cfg/main-items';
 import './index.scss';
+
 
 export default class Items extends Component {
     render () {
+        // const { title, image, price, add_stats, usage, description, notation } = mainItems;
         return (
             <div className="items-container">
                 <ItemInfo />
